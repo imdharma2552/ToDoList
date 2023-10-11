ccfd32646844bdfa7159b245a8b35aae9ec15583
Dharma <v.dharma2552@gamil.com>
2023-10-11 15:22:27 +0530

    first commit

---
+++ src/Components/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Contact = () => {
+    return (
+        <div>
+            <h1>My Contact Details</h1><br></br>
+            <p>+91 8688212019</p>
+            <p>v.dharma2552@gmail.com</p>
+        </div>
+    )
+}
+
+export default Contact
---
+++ src/Components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome To My WebPage</h1><br></br>
+            <p>This is Dharma Teja</p><br></br>
+            <span>HI ... !!!</span>
+        </div>
+    )
+}
+
+export default Home
